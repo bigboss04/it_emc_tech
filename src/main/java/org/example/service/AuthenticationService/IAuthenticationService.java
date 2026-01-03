634a19dfdca93d69ecec74e232e436c94a95a656
Dai pro <daipro@Dais-MacBook-Pro.local>
2026-01-03 09:34:31 +0700

    jwt buildScope

--- src/main/java/org/example/service/AuthenticationService/IAuthenticationService.java
+++ src/main/java/org/example/service/AuthenticationService/IAuthenticationService.java
@@ -1,4 +1,11 @@
 package org.example.service.AuthenticationService;
 
+import com.nimbusds.jose.JOSEException;
+import org.example.dto.request.IntrospectTokenRequest.IntrospectTokenRequest;
+import org.example.dto.response.IntrospectResponse;
+
+import java.text.ParseException;
+
 public interface IAuthenticationService {
+    IntrospectResponse introspect(IntrospectTokenRequest introspectTokenRequest) throws JOSEException, ParseException;
 }
