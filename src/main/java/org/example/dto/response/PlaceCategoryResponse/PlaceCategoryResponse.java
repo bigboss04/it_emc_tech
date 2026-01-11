ed98201672523b6c9eeff30077cca7b35aa4153d
Dai pro <daipro@Dais-MacBook-Pro.local>
2026-01-12 02:16:22 +0700

    add

---
+++ src/main/java/org/example/dto/response/PlaceCategoryResponse/PlaceCategoryResponse.java
@@ -0,0 +1,4 @@
+package org.example.dto.response.PlaceCategoryResponse;
+
+public class PlaceCategoryResponse {
+}
---
+++ src/main/java/org/example/service/PlaceTagService/PlaceTagService.java
@@ -0,0 +1,4 @@
+package org.example.service.PlaceTagService;
+
+public class PlaceTagService {
+}
